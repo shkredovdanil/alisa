5f1e3fa2d1a8fa1065e2f411b704113b8b67fa32
Danil <Danilshkr@gmail.com>
2023-03-13 18:28:33 +0700

    cities

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import logging
+import random
 
 import json
 
@@ -8,11 +9,18 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 logging.basicConfig(
     filename='example.log',
     format='%(asctime)s %(levelname)s %(name)s %(message)s',
-    level=logging.DEBUG
+    level=logging.INFO
 )
 
 sessionStorage = {}
-object = 'слон'
+cities = {
+    'москва': ['937455/7e163be94522dffaf4e8',
+               '937455/261fa113b58c38c866dd'],
+    'нью-йорк': ['1533899/c12ecb8ef146b91a0cf1',
+                 '1030494/ef21d5236c4ead6e79f6'],
+    'париж': ["1030494/62d4615d1365921382f9",
+              '1533899/2b2ee27b5a8458675edd']
+}
 
 
 @app.route('/post', methods=['POST'])
@@ -26,7 +34,6 @@ def main():
             'end_session': False
         }
     }
-    print(response)
     handle_dialog(request.json, response)
 
     logging.info(f'Response:  {response!r}')
@@ -35,71 +42,53 @@ def main():
 
 
 def handle_dialog(req, res):
-    global object
     user_id = req['session']['user_id']
-
     if req['session']['new']:
+        res['response']['text'] = 'Привет! Назови свое имя!'
         sessionStorage[user_id] = {
-            'suggests': [
-                "Не хочу.",
-                "Не буду.",
-                "Отстань!",
-            ]
+            'first_name': None
         }
-        res['response']['text'] = f'Привет! Купи {object}а!'
-        res['response']['buttons'] = get_suggests(user_id, object)
         return
-    if req['request']['original_utterance'].lower() in [
-        'ладно',
-        'куплю',
-        'покупаю',
-        'хорошо',
-        'я покупаю',
-        'я куплю'
-    ]:
-        if object != 'слон':
-            res['response']['text'] = f'Кролика можно найти на Яндекс.Маркете!'
-            res['response']['end_session'] = True
-            object = 'слон'
+    if sessionStorage[user_id]['first_name'] is None:
+        first_name = get_first_name(req)
+        if first_name is None:
+            res['response']['text'] = \
+                'Не расслышала имя. Повтори, пожалуйста!'
         else:
-            res['response']['text'] = f'Слона можно найти на Яндекс.Маркете!\n' \
-                                      f'А теперь купи кролика'
+            sessionStorage[user_id]['first_name'] = first_name
+            res['response'][
+                'text'] = 'Приятно познакомиться, ' \
+                          + first_name.title() \
+                          + '. Я - Алиса. Какой город хочешь увидеть?'
+            res['response']['buttons'] = [
+                {
+                    'title': city.title(),
+                    'hide': True
+                } for city in cities
+            ]
+    else:
+        city = get_city(req)
+        if city in cities:
+            res['response']['card'] = {}
+            res['response']['card']['type'] = 'BigImage'
+            res['response']['card']['title'] = 'Этот город я знаю.'
+            res['response']['card']['image_id'] = random.choice(cities[city])
+            res['response']['text'] = 'Я угадал!'
+        else:
+            res['response']['text'] = \
+                'Первый раз слышу об этом городе. Попробуй еще разок!'
 
-            object = 'кролик'
-            sessionStorage[user_id] = {
-                'suggests': [
-                    "Не хочу.",
-                    "Не буду.",
-                    "Отстань!",
-                ]
-            }
-            res['response']['buttons'] = get_suggests(user_id, object)
-        return
 
-    res['response']['text'] = \
-        f"Все говорят '{req['request']['original_utterance']}', а ты купи {object}а!"
-    res['response']['buttons'] = get_suggests(user_id, object)
+def get_city(req):
+    for entity in req['request']['nlu']['entities']:
+        if entity['type'] == 'YANDEX.GEO':
+            return entity['value'].get('city', None)
 
 
-def get_suggests(user_id, object):
-    session = sessionStorage[user_id]
-
-    suggests = [
-        {'title': suggest, 'hide': True}
-        for suggest in session['suggests'][:2]
-    ]
-
-    session['suggests'] = session['suggests'][1:]
-    sessionStorage[user_id] = session
-
-    if len(suggests) < 2:
-        suggests.append({
-            "title": "Ладно",
-            "url": f"https://market.yandex.ru/search?text={object}",
-            "hide": True
-        })
-
-    return suggests
+def get_first_name(req):
+    for entity in req['request']['nlu']['entities']:
+        if entity['type'] == 'YANDEX.FIO':
+            return entity['value'].get('first_name', None)
 
 
 if __name__ == '__main__':
